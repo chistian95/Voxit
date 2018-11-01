bc67bc26c87bc5c4015753ff2d26d8076832ab41
chistian95 <kriskorral59@gmail.com>
2018-11-01 23:36:20 +0100

    Pruebas

--- public/js/juego/init.js
+++ public/js/juego/init.js
@@ -9,21 +9,16 @@ var crearEscena = function() {
   camara.attachControl(canvas, true);
 
 
-  var luz1 = new BABYLON.HemisphericLight("luz1", new BABYLON.Vector3(1,1,0), scene);
-  luz1.position = new BABYLON.Vector3(0,3,0);
+  var luz1 = new BABYLON.HemisphericLight("luz1", new BABYLON.Vector3(0,1,0), scene);
+  var matBase = new BABYLON.StandardMaterial("basico", scene);
+  matBase.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
+  matBase.diffuseColor = new BABYLON.Color3(0.3, 0.7, 0.3);
 
   for(var x=-5; x<5; x++) {
     for(var z=-5; z<5; z++) {
-      var colores = [];
-      var red = Math.random();
-      var green = Math.random();
-      var blue = Math.random();
-      for(var i=0; i<6; i++) {
-        colores[i] = new BABYLON.Color4(red, green, blue, 0.1);
-      }
-
-      var cubo = BABYLON.MeshBuilder.CreateBox("cubo", {faceColors: colores}, scene);
+      var cubo = BABYLON.MeshBuilder.CreateBox("cubo", scene);
       cubo.position = new BABYLON.Vector3(x,0,z);
+      cubo.material = matBase;
     }
   }
 
