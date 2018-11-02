e5cfde26432130361d5a1414909f9e75ef251385
chistian95 <kriskorral59@gmail.com>
2018-11-02 15:19:02 +0100

    Optimizaciones

--- public/js/juego/init.js
+++ public/js/juego/init.js
@@ -13,14 +13,25 @@ var crearEscena = function() {
   var matAgua = new BABYLON.StandardMaterial("agua", scene);
   matAgua.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
   matAgua.diffuseColor = new BABYLON.Color3(0.1, 0.45, 0.82);
+  matAgua.freeze();
 
   var matGrass = new BABYLON.StandardMaterial("grass", scene);
   matGrass.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
   matGrass.diffuseColor = new BABYLON.Color3(0.3, 0.7, 0.3);
+  matGrass.freeze();
 
   var matPiedra = new BABYLON.StandardMaterial("piedra", scene);
   matPiedra.specularColor = new BABYLON.Color3(0.1, 0.1, 0.1);
   matPiedra.diffuseColor = new BABYLON.Color3(0.6, 0.6, 0.6);
+  matPiedra.freeze();
+
+  var materiales = {
+    agua: matAgua,
+    grass: matGrass,
+    piedra: matPiedra,
+  }
+
+  var meshes = {};
 
   scene.socket.on('cargarMapa', function(mapa) {
     for(var x=0,lenX=mapa.length; x<lenX; x++) {
@@ -28,13 +39,23 @@ var crearEscena = function() {
         for(var y=0,lenY=mapa[x][z].length; y<lenY; y++) {
           var val = mapa[x][z][y];
           if(val > 0) {
-            var cubo = BABYLON.MeshBuilder.CreateBox("c:"+x+";"+y+";"+z, scene);
-            cubo.position = new BABYLON.Vector3(x, y, z);
-            cubo.material = matPiedra;
-
+            var material = "piedra";
             if(val == 2) {
-              cubo.material = matAgua;
+              material = "agua";
             }
+
+            var cubo;
+            if(!meshes[material]) {
+              cubo = BABYLON.MeshBuilder.CreateBox("c:"+x+";"+y+";"+z, scene);
+              cubo.convertToUnIndexedMesh();
+              cubo.material = materiales[material];
+
+              meshes[material] = cubo;
+            } else {
+              cubo = meshes[material].createInstance("c:"+x+";"+y+";"+z);
+            }
+
+            cubo.position = new BABYLON.Vector3(x, y, z);
           }
         }
       }
